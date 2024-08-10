388fc122d0c7568e4918bd68b91d05b1a87b62d8
Elena Kolesnikovich <solnechnaya_88@list.ru>
2024-08-10 17:18:37 +0200

    installed eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,11 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-  }
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
+  },
+  plugins: ["jest"]
 }
--- spec/sum.test.js
+++ spec/sum.test.js
@@ -1,6 +1,6 @@
 import { sum } from '../src/sum.js'
 
-describe(`sum`, () => {
+describe("sum", () => {
     test(` sum of 1 and 2 returns 3`, () => {
         expect(sum(1, 2)).toBe(3);
     })
